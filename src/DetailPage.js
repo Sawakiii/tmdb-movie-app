fd48dc6f9740e607d77be90fa5dc079890e485a2
Sawaki <you@example.com>
2020-04-02 21:07:42 +0900

    Pagination機能

--- src/DetailPage.js
+++ src/DetailPage.js
@@ -1,14 +1,13 @@
 import React from "react"
-import Pagination from '@material-ui/lab/Pagination'
+import { Link, Route, BrowserRouter } from "react-router-dom"
+import MovieDetail from "./MovieDetail"
 
-
-const App = (props) => {
+const DetailPage = (props) => {
   // 映画データの定義
-  const [movies, setMovies] = React.useState([])
-  const [page, setPage] = React.useState()
+  const movies = props.movies
+  const setMovies = props.setMovies
 
-  // ページ切替時のために検索ワードを保存しておく。デフォルトでdieで検索しているのでdieを初期値にする
-  const [searchValue, setSearchValue] = React.useState("die")
+  // 検索ワードをstateにする必要はない
 
   // APIキーを格納
   const API_KEY = "11474ff130f2d229944829565a279ac3"
@@ -20,7 +19,6 @@ const App = (props) => {
     .then(res=>{
       console.log(res.results)
       setMovies(res.results)
-      setPage(res.total_pages)
     })
   }, [])
 
@@ -28,10 +26,6 @@ const App = (props) => {
   // 検索機能
   const handleSearch = (e) => {
     e.preventDefault()
-
-    // ページ変化時のために検索ワードを保存
-    setSearchValue(e.target.searchInput.value)
-
     fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${e.target.searchInput.value}&page=1&include_adult=false`)
     .then(res=>res.json())
     .then(res=>{
@@ -41,19 +35,8 @@ const App = (props) => {
   }
   
   // ページ切替機能
-  const handlePage = page => {
-    // 動作するかコンソールで確認
-    console.log(page)
 
-    // ページ切替時に別ページの内容をGETする
-    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${searchValue}}&page=${page}&include_adult=false`)
-    .then(res=>res.json())
-    .then(res=>{
-      console.log(res.results)
-      setMovies(res.results)
-    })
-  }
-
+  // 詳細を見る機能 押すと全体が
 
 
   return (
@@ -68,19 +51,25 @@ const App = (props) => {
         { movies.map((movie, i)=>{
           return (
             <li>
-              <p>----------------------</p>
-              <h1>{movie.title}</h1>
+              {/* <Link to={`/${movie.id}`}>{movie.title} */}
+              {/* <h1>{movie.title}</h1> */}
+              {/* </Link> */}
+              
+              {/* <Route path="/:id">
+                <MovieDetail movie={movie}></MovieDetail>
+              </Route> */}
+              {/* <BrowserRouter>
+              <Route path="/:id" render={(props)=><MovieDetail movie={movie}/>} />
+              </BrowserRouter> */}
               <img src={"https://image.tmdb.org/t/p/w185" + movie.poster_path} alt=""/>
               <p>{movie.overview}</p>
-              
             </li>
           )
         }) }
       </ul>
-      <Pagination count={page} onChange={(e, page)=>{handlePage(page)}}></Pagination>
     </>
   )
 }
 
 
-export default App
+export default DetailPage
---
+++ src/title.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const title = (props) => {
+    return (
+        <>
+            <h1>yesyese</h1>
+        </>
+    )
+}
+
+
+
+export default title
